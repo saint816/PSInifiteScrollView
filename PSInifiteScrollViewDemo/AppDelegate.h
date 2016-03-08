0da2f2b90c74fc9796535aaaeba75dbc623df01f
saint816 <pengsheng816@qq.com>
2016-03-08 14:11:33 +0800

    初始化项目结构

---
+++ PSInifiteScrollViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  03-0使用UIScrollView制作无限滚动
+//
+//  Created by 彭圣 on 16/3/5.
+//  Copyright © 2016年 PS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
